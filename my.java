d56dfe28833fee97c212daaa879078a4bffc7a61
Mayashankar Mishra <mayashankar_m@yahoo.com>
2020-11-22 17:18:08 +0100

    Added my.java
    Signed-off-by: Mayashankar Mishra <mayashankar_m@yahoo.com>

---
+++ my.java
@@ -0,0 +1,18 @@
+public class my{
+
+   public static void main(String[] args){
+
+       for(int i=0;i<=7;i++){
+
+        System.out.println("Hello World..."+i);
+
+
+  
+       }
+
+
+   }
+
+
+
+}
